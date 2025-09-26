d2dc5efe6c95b83d90261380b3decc9c88a5f86c
Shoruda <axelbergman@gmail.comgit init axel-dev-Atouch program.txttouch .gitignoregit addgit config --global user.email axelbergman@gmail.com>
2025-09-26 15:47:54 +0200

    tcp almost done, fork works i think?

--- tcpservermain.cpp
+++ tcpservermain.cpp
@@ -11,6 +11,7 @@
 #include <calcLib.h>
 #include <errno.h>
 
+#include "calcLib.h"
 // Enable if you want debugging to be printed, see examble below.
 // Alternative, pass CFLAGS=-DDEBUG to make, make CFLAGS=-DDEBUG
 #define DEBUG
@@ -20,116 +21,153 @@
 using namespace std;
 
 
+// ... all includes and setup as in your original code ...
+
 int main(int argc, char *argv[]){
-  if (argc < 2) {
-    fprintf(stderr, "Usage: %s protocol://server:port/path.\n", argv[0]);
-    exit(EXIT_FAILURE);
-  }
-  
-  char *input = argv[1];
-  char *sep = strchr(input, ':');
-  
-  if (!sep) {
-    fprintf(stderr, "Error: input must be in host:port format\n");
-    return 1;
-  }
-  
-  // Allocate buffers big enough
-  char hoststring[256];
-  char portstring[64];
-  
-  // Copy host part
-  size_t hostlen = sep - input;
-  if (hostlen >= sizeof(hoststring)) {
-    fprintf(stderr, "Error: hostname too long\n");
-    return 1;
-  }
-  strncpy(hoststring, input, hostlen);
-  hoststring[hostlen] = '\0';
-  
-  // Copy port part
-  strncpy(portstring, sep + 1, sizeof(portstring) - 1);
-  portstring[sizeof(portstring) - 1] = '\0';
-  
-  printf("TCP server on: %s:%s\n", hoststring,portstring);
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s host:port\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-  // DO MAGIC :3
+    char *input = argv[1];
+    char *sep = strchr(input, ':');
+    if (!sep) {
+        fprintf(stderr, "Error: input must be in host:port format\n");
+        return 1;
+    }
 
-  struct addrinfo hints, *res, *p;
-  int status;
+    char hoststring[256];
+    char portstring[64];
+    size_t hostlen = sep - input;
+    strncpy(hoststring, input, hostlen);
+    hoststring[hostlen] = '\0';
+    strncpy(portstring, sep + 1, sizeof(portstring) - 1);
+    portstring[sizeof(portstring) - 1] = '\0';
 
-  memset(&hints, 0, sizeof hints);
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_PASSIVE;
+    printf("TCP server on: %s:%s\n", hoststring, portstring);
 
-  status = getaddrinfo(hoststring, portstring, &hints, &res);
-  if (status != 0) {
-    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
-    exit(1);
-  }
+    struct addrinfo hints, *res, *p;
+    int status;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
 
-  int sockfd;
-  for (p = res; p != NULL; p = p->ai_next) {
-    sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
-    if (sockfd == -1)
-        continue;
-
-    int yes = 1;
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-        fprintf(stderr, "setsockopt error: %s\n", strerror(errno));
+    status = getaddrinfo(hoststring, portstring, &hints, &res);
+    if (status != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
         exit(1);
     }
 
-    if (bind(sockfd, p->ai_addr, p->ai_addrlen) == 0) {
-        break;
+    int sockfd;
+    for (p = res; p != NULL; p = p->ai_next) {
+        sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (sockfd == -1) continue;
+
+        int yes = 1;
+        setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == 0) break;
+
+        close(sockfd);
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "Failed to bind socket\n");
+        exit(1);
+    }
+
+    freeaddrinfo(res);
+
+    if (listen(sockfd, BACKLOG) == -1) {
+        fprintf(stderr, "listen error: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    printf("Server is listening...\n");
+
+    while (1) {
+        struct sockaddr_storage cli_addr;
+        socklen_t clilen = sizeof(cli_addr);
+
+        int newsock = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+        if (newsock == -1) {
+            fprintf(stderr, "accept error: %s\n", strerror(errno));
+            continue; // parent continues
+        }
+
+        pid_t pid = fork();
+        if (pid < 0) {
+            fprintf(stderr, "fork error: %s\n", strerror(errno));
+            close(newsock);
+            continue;
+        }
+
+        if (pid == 0) {
+            // Child process
+            close(sockfd); // child doesn't need listening socket
+
+            printf("Client connected!\n");
+
+            const char *msg = "TEXT TCP 1.1\n";
+            send(newsock, msg, strlen(msg), 0);
+
+            char buffer[256];
+            int n = recv(newsock, buffer, sizeof(buffer) - 1, 0);
+            if (n < 0) {
+                fprintf(stderr, "recv error: %s\n", strerror(errno));
+                close(newsock);
+                exit(1);
+            }
+            buffer[n] = '\0';
+            printf("Got message: %s\n", buffer);
+
+            initCalcLib();
+            char* operation = randomType();
+            int value1 = randomInt();
+            int value2 = randomInt();
+
+            int correct_result;
+            if (strcmp(operation, "add") == 0) correct_result = value1 + value2;
+            else if (strcmp(operation, "sub") == 0) correct_result = value1 - value2;
+            else if (strcmp(operation, "mul") == 0) correct_result = value1 * value2;
+            else if (strcmp(operation, "div") == 0) correct_result = value1 / value2;
+            else correct_result = 0;
+
+            snprintf(buffer, sizeof(buffer), "%s %d %d\n", operation, value1, value2);
+            send(newsock, buffer, strlen(buffer), 0);
+            printf("Sent assignment: %s %d %d\n", operation, value1, value2);
+
+            n = recv(newsock, buffer, sizeof(buffer) - 1, 0);
+            if (n < 0) {
+                fprintf(stderr, "recv error: %s\n", strerror(errno));
+                close(newsock);
+                exit(1);
+            }
+            buffer[n] = '\0';
+            printf("answer: %s\n", buffer);
+
+            int client_result = atoi(buffer);
+            if (client_result == correct_result) {
+                const char *okmsg = "OK\n";
+                send(newsock, okmsg, strlen(okmsg), 0);
+                printf("Client was correct! (%d)\n", client_result);
+            } else {
+                const char *errmsg = "ERROR\n";
+                send(newsock, errmsg, strlen(errmsg), 0);
+                printf("Client was wrong! Expected %d but got %d\n", correct_result, client_result);
+            }
+
+            close(newsock);
+            exit(0);
+        } else {
+            // Parent process
+            close(newsock);
+        }
     }
 
     close(sockfd);
-  }
-
-  if (p == NULL) {
-    fprintf(stderr, "Failed to bind socket\n");
-    exit(1);
-  }
-
-  freeaddrinfo(res);
-
-  //listen
-
-  if (listen(sockfd, BACKLOG) == -1) {
-    fprintf(stderr, "listen error: %s\n", strerror(errno));
-    exit(1);
-  }
-
-  printf("Server is listening...\n");
-
-  //accept
-
-  struct sockaddr_storage cli_addr;
-  socklen_t clilen = sizeof(cli_addr);
-
-  int newsock = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
-  if (newsock == -1) {
-    fprintf(stderr, "accept error: %s\n", strerror(errno));
-    exit(1);
-  }
-
-  printf("Client connected!\n");
-
-  char buffer[256];
-  int n = recv(newsock, buffer, sizeof(buffer) - 1, 0);
-  if (n < 0) {
-    fprintf(stderr, "recv error: %s\n", strerror(errno));
-    exit(1);
-  }
-  buffer[n] = '\0';
-  printf("Got message: %s\n", buffer);
-
-  const char *msg = "Hello from TCP server";
-  send(newsock, msg, strlen(msg), 0);
-
-  close(newsock);
-  close(sockfd);
-  return 0;
+    return 0;
 }
+
+
